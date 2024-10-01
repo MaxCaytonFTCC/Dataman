35e796a24be3d6fb36c6bd7b60d0412beb3bff5d
MaxCaytonFTCC <90518913+MaxCaytonFTCC@users.noreply.github.com>
2024-10-01 13:12:40 +0000

    Added More HTML Templates, Bootstrap & Routes

--- dataman/main.py
+++ dataman/main.py
@@ -7,9 +7,36 @@ app.config["DEBUG"] = True
 @app.route("/", methods=["GET", "POST"])
 def index():
     
+
+    # Or Redirect to itself during POST (or other)
+    return redirect(url_for('operand1'))
+
+@app.route("/operand1")
+def operand1():
+    
     # Either show the page...
     if request.method == "GET":
-        return render_template("main_page.html")
+        return render_template("operand1.html")
+
+    # Or Redirect to itself during POST (or other)
+    return redirect(url_for('index'))
+
+@app.route("/operation")
+def operation():
+    
+    # Either show the page...
+    if request.method == "GET":
+        return render_template("operation.html")
+
+    # Or Redirect to itself during POST (or other)
+    return redirect(url_for('index'))
+
+@app.route("/operand2")
+def operand2():
+    
+    # Either show the page...
+    if request.method == "GET":
+        return render_template("operand2.html")
 
     # Or Redirect to itself during POST (or other)
     return redirect(url_for('index'))
