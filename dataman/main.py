e94cce01aca8db44d8bdf4ea48c533b0608d7fda
MaxCaytonFTCC <90518913+MaxCaytonFTCC@users.noreply.github.com>
2024-10-02 13:18:55 +0000

    Added Input Form with POST Requests

--- dataman/main.py
+++ dataman/main.py
@@ -6,22 +6,24 @@ app.config["DEBUG"] = True
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    
-
     # Or Redirect to itself during POST (or other)
     return redirect(url_for('operand1'))
 
-@app.route("/operand1")
+@app.route("/operand1", methods=["GET", "POST"])
 def operand1():
     
-    # Either show the page...
-    if request.method == "GET":
-        return render_template("operand1.html")
+    if (request.method == "POST"):
+        currentEquation = request.form['display']
+        print(f"Received value: {currentEquation}")
+        
+        return render_template('operand1.html', operand1=currentEquation)
+    return render_template('operand1.html', operand1='')
+    
 
-    # Or Redirect to itself during POST (or other)
+    # Redirect to Operand2
     return redirect(url_for('index'))
 
-@app.route("/operation")
+@app.route("/operation", methods=["GET", "POST"])
 def operation():
     
     # Either show the page...
@@ -31,7 +33,7 @@ def operation():
     # Or Redirect to itself during POST (or other)
     return redirect(url_for('index'))
 
-@app.route("/operand2")
+@app.route("/operand2", methods=["GET", "POST"])
 def operand2():
     
     # Either show the page...
