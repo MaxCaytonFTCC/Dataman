75d591f84e6c4f95047e12762c1b90042e58f732
MaxCaytonFTCC <90518913+MaxCaytonFTCC@users.noreply.github.com>
2024-10-02 16:10:27 +0000

    Simplified Pages

--- dataman/main.py
+++ dataman/main.py
@@ -7,38 +7,13 @@ app.config["DEBUG"] = True
 @app.route("/", methods=["GET", "POST"])
 def index():
     # Or Redirect to itself during POST (or other)
-    return redirect(url_for('operand1'))
+    return redirect(url_for('calculator'))
 
-@app.route("/operand1", methods=["GET", "POST"])
-def operand1():
-    
+@app.route("/calculator", methods=["GET", "POST"])
+def calculator():
     if (request.method == "POST"):
         currentEquation = request.form['display']
         print(f"Received value: {currentEquation}")
         
-        return render_template('operand1.html', operand1=currentEquation)
-    return render_template('operand1.html', operand1='')
-    
-
-    # Redirect to Operand2
-    return redirect(url_for('index'))
-
-@app.route("/operation", methods=["GET", "POST"])
-def operation():
-    
-    # Either show the page...
-    if request.method == "GET":
-        return render_template("operation.html")
-
-    # Or Redirect to itself during POST (or other)
-    return redirect(url_for('index'))
-
-@app.route("/operand2", methods=["GET", "POST"])
-def operand2():
-    
-    # Either show the page...
-    if request.method == "GET":
-        return render_template("operand2.html")
-
-    # Or Redirect to itself during POST (or other)
-    return redirect(url_for('index'))
+        return render_template('calculator.html', currentEquation=currentEquation)
+    return render_template('calculator.html', currentEquation='')
