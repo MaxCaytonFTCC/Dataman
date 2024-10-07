a0a03d5522f3f1436da352f2263181801c08b7f3
carterj7383 <156561727+carterj7383@users.noreply.github.com>
2024-10-07 11:46:43 +0000

    update main.py

--- dataman/main.py
+++ dataman/main.py
@@ -1,15 +1,22 @@
 # Minimal Flask App
 from flask import Flask, render_template, request, redirect, url_for
+import answer_checker as answ
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    
-    # Either show the page...
-    if request.method == "GET":
-        return render_template("calculator.html")
+    return redirect(url_for('calculator'))
 
-    # Or Redirect to itself during POST (or other)
-    return redirect(url_for('index'))
+@app.route("/calculator", methods=["GET", "POST"])
+def calculator():
+    if (request.method == "POST"):
+        currentEquation = request.form['display']
+        
+        # Check Answer & Place response in Calculator Display
+        currentEquation = 'You got it!' if answ.checkAnswer(currentEquation) else 'EEEEEE'
+        return render_template('calculator.html', currentEquation=currentEquation)
+    
+    # GET Response
+    return render_template('calculator.html', currentEquation='')
